5c91fbc58415302ae8f8e3f93c612d9f2235f6d7
Gianni Antonio Manilia <giannimanilia@gmail.com>
2021-02-14 17:48:53 +0100

    SS-1 removed warnings

--- app/src/main/java/com/gmaniliapp/sudokusolver/exception/IllegalSudokuContentException.kt
+++ app/src/main/java/com/gmaniliapp/sudokusolver/exception/IllegalSudokuContentException.kt
@@ -2,5 +2,4 @@ package com.gmaniliapp.sudokusolver.exception
 
 import java.lang.IllegalArgumentException
 
-open class IllegalSudokuContentException(message:String): IllegalArgumentException(message) {
-}
+open class IllegalSudokuContentException(message:String): IllegalArgumentException(message)
--- app/src/main/java/com/gmaniliapp/sudokusolver/exception/IllegalSudokuDimensionException.kt
+++ app/src/main/java/com/gmaniliapp/sudokusolver/exception/IllegalSudokuDimensionException.kt
@@ -2,5 +2,4 @@ package com.gmaniliapp.sudokusolver.exception
 
 import java.lang.IllegalArgumentException
 
-class IllegalSudokuDimensionException(message:String): IllegalArgumentException(message) {
-}
+class IllegalSudokuDimensionException(message:String): IllegalArgumentException(message) 
